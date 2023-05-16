7085ae99ec89ee6e2ee16471009524faef7d68bc
Akbar <you@example.com>
2023-05-15 20:51:38 -0400

    group1

---
+++ src/first_group/Bank.java
@@ -0,0 +1,20 @@
+package first_group;
+
+public class Bank {
+    public static void main(String[] args) {
+        String sentence = "Judo is more than sport";
+
+        System.out.println(sentence.contains("is more"));  //true
+
+        System.out.println(sentence.contains("morethan"));  //false
+
+        System.out.println(sentence. contains("is than"));  //false
+
+
+        System.out.println(sentence.contains("is") && sentence.contains("than"));  // true
+        System.out.println(sentence.contains("is") || sentence.contains("than"));  // true
+        System.out.println(sentence.contains("are") || sentence.contains("than"));  // true
+        System.out.println(sentence.contains("is") && sentence.contains("the"));  // false
+    }
+
+}
